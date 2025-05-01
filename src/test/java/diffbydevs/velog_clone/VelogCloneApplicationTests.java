e701caab11ef2d232a7d68114dfe82d018804141
Sooamazing <honang515@gmail.com>
2025-05-02 01:15:51 +0900

    feat: no dependency 초기 구

---
+++ src/test/java/diffbydevs/velog_clone/VelogCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package diffbydevs.velog_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
