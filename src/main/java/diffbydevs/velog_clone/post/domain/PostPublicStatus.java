456825a9ce26962dc35cb4fdb332cc04e739e983
Sooamazing <honang515@gmail.com>
2025-05-13 06:48:49 +0900

    [#13]Feat(Post): PostSetting Entity, PostPublicStatus, 생성자 대신 정적팩토리 메서드 사용
    boolean이지만 db 관리 data type 차이로 enum으로 변환 관리

---
+++ src/main/java/diffbydevs/velog_clone/post/domain/PostPublicStatus.java
@@ -0,0 +1,31 @@
+package diffbydevs.velog_clone.post.domain;
+
+import lombok.Getter;
+
+/**
+ * 게시글의 공개 상태를 나타냅니다.
+ * <p>
+ *     코드의 가독성을 위해 어플리케이션 코드에서만 사용합니다.
+ *     실제 db에서는 정규화 문제로 ENUM이 아닌 정수형으로 관리합니다.
+ * </p>
+ */
+@Getter
+public enum PostPublicStatus {
+	/**
+	 * 비공개
+	 */
+	PRIVATE(0),
+	/**
+	 * 공개
+	 */
+	PUBLIC(1);
+
+	/**
+	 * 실제로 db에 들어가게 되는 값
+	 */
+	private int valueInDB;
+
+	PostPublicStatus(int valueInDB) {
+		this.valueInDB = valueInDB;
+	}
+}
---
+++ src/main/java/diffbydevs/velog_clone/post/domain/PostSetting.java
@@ -0,0 +1,103 @@
+package diffbydevs.velog_clone.post.domain;
+
+import diffbydevs.velog_clone.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+TODO
+1. brieflyIntroduce 는 여기서 검증하면 글 내용 불러와야 하니까 이전에 확인하고 가져오기로.
+2. slug 중복 확인 후, 있는 거면 -UUID(6)로 slug 저장하는 로직은 어디에 둘지 결정 필요.
+ */
+
+/**
+ * 게시글의 환경 설정을 관리합니다.
+ */
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PostSetting extends BaseEntity {
+
+	@Id
+	@Column(columnDefinition = "int unsigned")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_published_info_id")
+	private PostPublishedInfo postPublishedInfo;
+
+	/**
+	 * 글 제목
+	 */
+	@NotNull
+	@Size(max = 100)
+	@Column(length = 100)
+	private String title;
+
+	/**
+	 * 미리 보기에서 보여주는 글에 대한 짧은 소개
+	 */
+	@NotNull
+	@Size(max = 150)
+	@Column(length = 150)
+	private String brieflyIntroduce;
+
+	/**
+	 * 공개, 비공개 설정
+	 */
+	@NotNull
+	@Column(columnDefinition = "tinyint(1) unsigned")
+	private int isPublic;
+
+	/*
+	 * 글의 고유 slug.
+	 * 한글, 영어 소문자, 숫자, - 허용합니다.
+	 * 작성하지 않으면, 기본으로 제목으로 시도합니다.
+	 */
+	@NotNull
+	@Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-]*$")
+	@Size(max = 110)
+	@Column(unique = true, length = 110)
+	private String slug;
+
+	/**
+	 * 게시글 환경을 설정합니다.
+	 *
+	 * @param title 글 제목
+	 * @param brieflyIntroduce 글 간단 소개
+	 * @param postPublicStatus 공개/비공개
+	 * @param slug uri에 사용하는 slug
+	 * @return 게시글 환경 설정
+	 */
+	public static PostSetting setThePostEnvironment(
+		String title,
+		String brieflyIntroduce,
+		PostPublicStatus postPublicStatus,
+		String slug
+	) {
+		PostSetting postSetting = new PostSetting();
+
+		postSetting.title = title;
+		postSetting.brieflyIntroduce = brieflyIntroduce;
+		postSetting.isPublic = postPublicStatus.getValueInDB();
+		postSetting.slug = slug;
+
+		return postSetting;
+	}
+}
