e216308b553e4dbe5868aab7ac500cfdc5214ddf
Sooamazing <honang515@gmail.com>
2025-05-13 06:13:55 +0900

    [#13]Feat(Post): Post의 메타 데이터를 저장하는 PostPublishedInfo Entity, PostPublishedStatus  생성
    - 임시 저장/발행, 시리즈 유/무에 따라 로직 변화.
    - 생성자 대신 정적팩토리 메서드 사용
    - Member와 동일하게 Status는 db와 달리 코드에서 Enum 관리

---
+++ src/main/java/diffbydevs/velog_clone/post/domain/PostPublishedInfo.java
@@ -0,0 +1,123 @@
+package diffbydevs.velog_clone.post.domain;
+
+import java.time.LocalDateTime;
+
+import diffbydevs.velog_clone.blog.domain.Blog;
+import diffbydevs.velog_clone.member.domain.Member;
+import diffbydevs.velog_clone.series.domain.Series;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+공부
+TODO
+1. 이거 null인 것도 내가… 생성자에서 꼭 설정을 해 줘야 했나..?
+2. publishedAt -> not null query 확인 필요
+ */
+
+/**
+ * 게시글의 근간이 됩니다.
+ * 연관 관계, 발행 관련 정보를 가지고 있습니다.
+ * 외부 통신 시 해당 Entity의 id로 소통합니다.
+ */
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PostPublishedInfo {
+
+	@Id
+	@Column(columnDefinition = "int unsigned")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "blog_id")
+	private Blog blog;
+
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "author_id")
+	private Member author;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "series_id")
+	private Series series;
+
+	@Column(columnDefinition = "tinyint(1) unsigned")
+	private int publishedStatus;
+
+	/**
+	 * 발행일. 임시 저장이면 발행일은 없고, 발행 이후엔 변경 불가.
+	 */
+	private LocalDateTime publishedAt;
+
+	/**
+	 * 시리즈에 포함된 게시글을 임시 저장 혹은 발행한다.
+	 *
+	 * @param blog 글을 작성할 블로그
+	 * @param author 글을 작성한 회원
+	 * @param series 글이 소속될 시리즈
+	 * @param publishedStatus 발행 상태
+	 * @return 게시글의 발행 정보
+	 */
+	public static PostPublishedInfo createPost(
+		Blog blog,
+		Member author,
+		Series series,
+		PostPublishedStatus publishedStatus
+	) {
+		PostPublishedInfo postPublishedInfo = new PostPublishedInfo();
+
+		postPublishedInfo.blog = blog;
+		postPublishedInfo.author = author;
+		postPublishedInfo.series = series;
+		postPublishedInfo.publishedStatus = publishedStatus.getValueInDB();
+		postPublishedInfo.publishedAt = null;
+		if (publishedStatus == PostPublishedStatus.PUBLISHED) {
+			postPublishedInfo.publishedAt = LocalDateTime.now();
+		}
+
+		return postPublishedInfo;
+	}
+
+	/**
+	 * 시리즈에 포함되지 않은 게시글을 임시 저장 혹은 발행한다.
+	 *
+	 * @param blog 글을 작성할 블로그
+	 * @param author 글을 작성한 회원
+	 * @param publishedStatus 발행 상태
+	 * @return 게시글의 발행 정보
+	 */
+	public static PostPublishedInfo createPost(
+		Blog blog,
+		Member author,
+		PostPublishedStatus publishedStatus
+	) {
+		PostPublishedInfo postPublishedInfo = new PostPublishedInfo();
+
+		postPublishedInfo.blog = blog;
+		postPublishedInfo.author = author;
+		postPublishedInfo.series = null;
+		postPublishedInfo.publishedStatus = publishedStatus.getValueInDB();
+		postPublishedInfo.publishedAt = null;
+
+		if (publishedStatus == PostPublishedStatus.PUBLISHED) {
+			postPublishedInfo.publishedAt = LocalDateTime.now();
+		}
+
+		return postPublishedInfo;
+	}
+}
---
+++ src/main/java/diffbydevs/velog_clone/post/domain/PostPublishedStatus.java
@@ -0,0 +1,31 @@
+package diffbydevs.velog_clone.post.domain;
+
+import lombok.Getter;
+
+/**
+ * 게시글의 발행 상태를 나타냅니다.
+ * <p>
+ *     코드의 가독성을 위해 어플리케이션 코드에서만 사용합니다.
+ *     실제 db에서는 정규화 문제로 ENUM이 아닌 정수형으로 관리합니다.
+ * </p>
+ */
+@Getter
+public enum PostPublishedStatus {
+	/**
+	 * 임시 저장
+	 */
+	DRAFT(0),
+	/**
+	 * 발행
+	 */
+	PUBLISHED(1);
+
+	/**
+	 * 실제로 db에 들어가게 되는 값
+	 */
+	private int valueInDB;
+
+	PostPublishedStatus(int valueInDB) {
+		this.valueInDB = valueInDB;
+	}
+}
