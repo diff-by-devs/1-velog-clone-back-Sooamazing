54f82b26ea2c375e70ec6c53826ae7837fe3c3c3
Sooamazing <honang515@gmail.com>
2025-05-13 00:54:34 +0900

    [#13]Feat(Member): Member Entity(member.user 테이블), MemberStatus 생성.

--- src/main/java/diffbydevs/velog_clone/member/domain/Member.java
+++ src/main/java/diffbydevs/velog_clone/member/domain/Member.java
@@ -1,4 +1,53 @@
 package diffbydevs.velog_clone.member.domain;
 
-public class Member {
+import diffbydevs.velog_clone.global.domain.BaseEntityOnlyUpdatedAt;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+TODO
+- query에서 Status에 check 제약도 함께 걸리는지 확인 필요.
+- 회원 CRUD 진행 시 Profile 등 생성 방식 고민 필요.
+ */
+/**
+ * 회원 Entity 중 근간이 되는 Entity. 
+ * <p>
+ *     회원에 대해 외부와 통신 시 해당 id로 통신합니다.
+ * </p>
+ */
+@Getter
+@Entity
+@Table(name = "member.user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member extends BaseEntityOnlyUpdatedAt {
+	@Id
+	@Column(columnDefinition = "int unsigned")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(columnDefinition = "tinyint(1) unsigned default 0")
+	private int status;
+
+	/**
+	 * Member를 생성하는(회원 가입하는) 정적 팩토리 메서드.
+	 * <p>
+	 *     Member 생성은 오직 이 메서드로만 가능합니다.
+	 * </p>
+	 * @return Member 회원 가입한 Member
+	 */
+	public static Member signUp() {
+		Member member = new Member();
+		member.status = MemberStatus.ACTIVE.getValueInDB();
+		return member;
+	}
+
 }
---
+++ src/main/java/diffbydevs/velog_clone/member/domain/MemberStatus.java
@@ -0,0 +1,35 @@
+package diffbydevs.velog_clone.member.domain;
+
+import lombok.Getter;
+
+/**
+ * 회원의 활성 상태를 나타냅니다.
+ * <p>
+ *     코드의 가독성을 위해 어플리케이션 코드에서만 사용합니다.
+ *     실제 db에서는 정규화 문제로 ENUM이 아닌 정수형으로 관리합니다.
+ * </p>
+ */
+@Getter
+public enum MemberStatus {
+	/**
+	 * 활성 상태, 회원 가입 이후, 휴면/탈퇴 전까지의 상태를 나타냅니다.
+	 */
+	ACTIVE(0),
+	/**
+	 * 휴면 상태
+	 */
+	DORMANT(1),
+	/**
+	 * 탈퇴 상태
+	 */
+	WITHDRAWAL(2);
+
+	/**
+	 * 실제로 db에 들어가게 되는 값
+	 */
+	private final int valueInDB;
+
+	MemberStatus(int valueInDB) {
+		this.valueInDB = valueInDB;
+	}
+}
