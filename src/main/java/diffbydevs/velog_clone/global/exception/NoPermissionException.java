e7b10e5d865ce7abe74b4cb48b55d5c0379a242e
Sooamazing <honang515@gmail.com>
2025-05-16 00:58:57 +0900

    [#15]Feat(Exception): 403 처리하기 위한 NoPermissionException 생성

---
+++ src/main/java/diffbydevs/velog_clone/global/exception/NoPermissionException.java
@@ -0,0 +1,12 @@
+package diffbydevs.velog_clone.global.exception;
+
+/**
+ * 특정 권한이 필요한 경우 발생하는 예외입니다.
+ * 기본적으로 HTTP 상태 코드 403에 대응됩니다.
+ * @see org.springframework.http.HttpStatus#FORBIDDEN
+ */
+public class NoPermissionException extends BaseException {
+	public NoPermissionException(String param, String errorMessage) {
+		super(GlobalErrorCode.NO_PERMISSION, param, errorMessage);
+	}
+}
