945caab02343ca5f4f56eb3c7ddcbfaf9da53718
Sooamazing <honang515@gmail.com>
2025-05-16 00:44:02 +0900

    [#15]Feat(Exception): 공통으로 구현하기 위해 ErrorCode interface로 구현

---
+++ src/main/java/diffbydevs/velog_clone/global/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package diffbydevs.velog_clone.global.exception;
+
+/**
+ * ErrorCode 유형을 관리하기 위한 에러 코드 인터페이스입니다.
+ * enum에서 상속받아 사용합니다.
+ */
+public interface ErrorCode {
+	String name();
+
+	String getMessage();
+}
