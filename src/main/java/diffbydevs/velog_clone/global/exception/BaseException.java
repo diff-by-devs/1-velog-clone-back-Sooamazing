7c2b655248333973fe203c10c45c5c245bbfaba2
Sooamazing <honang515@gmail.com>
2025-05-16 00:44:29 +0900

    [#15]Feat(Exception): 어플리케이션에서 발생하는 예외 처리 위해 BaseException 생성
    - Unchecked 에러만 처리 위해 Runtime 상속
    - 가급적 예외를 명확하게 하기 위해 BaseException 그대로가 아니라 상속받아 각 도메인에서 구현하도록 함.

--- src/main/java/diffbydevs/velog_clone/global/exception/BaseException.java
+++ src/main/java/diffbydevs/velog_clone/global/exception/BaseException.java
@@ -1,4 +1,23 @@
 package diffbydevs.velog_clone.global.exception;
 
-public class BaseException {
+import lombok.Getter;
+
+/*
+TODO Exception, RuntimeException 두 개의 차이는? 만약 Exception 상속 받았따면 어떤 게 달라질지?
+ */
+
+/**
+ * 어플리케이션에서 발생하는 예외를 처리하기 위한 기본 클래스입니다.
+ * 어플리케이션 예외를 명확하게 하기 위해 상속받아 사용합니다.
+ */
+@Getter
+public abstract class BaseException extends RuntimeException {
+	private final ErrorCode errorCode;
+	private final String param;
+
+	protected BaseException(ErrorCode errorCode, String param, String errorMessage) {
+		super(errorMessage);
+		this.errorCode = errorCode;
+		this.param = param;
+	}
 }
