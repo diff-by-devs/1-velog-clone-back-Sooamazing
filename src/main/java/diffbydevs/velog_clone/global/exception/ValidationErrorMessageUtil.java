5c7987825c98d0bc5bf98cb179518ae8ff092c64
Sooamazing <honang515@gmail.com>
2025-05-21 18:34:35 +0900

    [#15]Rename(Exception): 불변 map인 걸 알 수 있도록 변수명에 immutable 추가
    - TODO runtime에 error 발생하지 않도록 설계하는 법 찾아보기.

--- src/main/java/diffbydevs/velog_clone/global/exception/ValidationErrorMessageUtil.java
+++ src/main/java/diffbydevs/velog_clone/global/exception/ValidationErrorMessageUtil.java
@@ -1,6 +1,11 @@
 package diffbydevs.velog_clone.global.exception;
 
 import java.util.Map;
+/*
+TODO
+1. Guava ImmutableMap 사용 고려?
+2. Map.of 코드를 잘못 적으면 RUNTIME ERRor 발생하는데, 코드로 사용하지 못하게 해결할 수 있는 방법? 설계?
+ */
 
 /**
  * 공통으로 사용되는 Validation Error Message를 관리합니다.
@@ -23,11 +28,14 @@ public final class ValidationErrorMessageUtil {
 	public static final String PATTERN_PASSWORD = "PatternPassword";
 	public static final String SIZE_WITH_FINAL_CONSONANTS = "SizeWithFinalConsonants";
 	public static final String SIZE_NO_FINAL_CONSONANTS = "SizeNoFinalConsonants";
+	private ValidationErrorMessageUtil() {
+	}
+
 	/**
-	 * Validation Error Message를 관리하는 Map입니다.
-	 * Map.ofEntries()를 사용하여 불변의 Map을 생성합니다.
+	 * Validation Error Message를 관리하는 불변 Map입니다.
+	 * 수정 시 UnSupportedOperationException이 발생합니다.
 	 */
-	public static final Map<String, String> messageMap = Map.ofEntries(
+	public static final Map<String, String> immutableMessageMap = Map.ofEntries(
 		Map.entry(EMAIL, "에 올바른 형식의 이메일 주소를 입력해 주세요."),
 		Map.entry(PATTERN_PASSWORD, "는 영문 대문자, 소문자, 숫자, 특수문자를 포함하여 8자 이상 입력해 주세요."),
 
@@ -47,7 +55,4 @@ public final class ValidationErrorMessageUtil {
 		Map.entry(SIZE_WITH_FINAL_CONSONANTS, "은 {min} ~ {max} 사이로 입력해 주세요."),
 		Map.entry(SIZE_NO_FINAL_CONSONANTS, "는 {min} ~ {max} 사이로 입력해 주세요.")
 	);
-
-	private ValidationErrorMessageUtil() {
-	}
 }
