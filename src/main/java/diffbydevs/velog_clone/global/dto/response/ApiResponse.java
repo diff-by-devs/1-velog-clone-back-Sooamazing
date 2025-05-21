86df7a8f0157743678dd8157f355227354ce412e
Sooamazing <honang515@gmail.com>
2025-05-21 15:33:55 +0900

    [#15]Feat(API): boolean 변수명 반영 위해 Boolean으로 변경, 응답 순서 지정, handler에서 사용 위해 errorResponse 내부 구현 대신 T로 받음
    - 변수명 그대로 반환하기 위해 boolean -> Boolean
    - JsonPropertyOrder로 응답 순서 지정
    - fail 시 error 응답도 generic으로 수정. GlobalExceptionHandler 내부에서만 관리해도 될 것 같아서.

--- src/main/java/diffbydevs/velog_clone/global/dto/response/ApiResponse.java
+++ src/main/java/diffbydevs/velog_clone/global/dto/response/ApiResponse.java
@@ -1,31 +1,37 @@
 package diffbydevs.velog_clone.global.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import diffbydevs.velog_clone.global.exception.ErrorCode;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /*
 TODO
-1. getter 빼고 만들어 보기. 역직렬화 확인.
+1. boolean + is 접두사인 경우, is 접두사는 생략됨 -> Boolean 말고 더 좋은 방식? Boolean으로 할 때, null 방지할 수 있는 방법?
 2. 왜 여기는 기본 생성자가 없어도 될까?
+3. results? errors? 단수 ? 복수? API마다는 객체/LIST 통일하는 게 낫겠지?
 */
 
 /**
  * 일관적인 Rest API 응답을 만듭니다.
  * 성공, 실패 여부에 따라 다른 형태로 응답합니다.
- * @param <T> 성공 응답 데이터 타입
+ * @param <T> API 요청의 성공 결과 또는 오류 내용을 담는 타입
  */
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+	"isSuccess",
+	"result",
+	"error"
+})
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 	/**
-	 * API 요청의 성공 여부
+	 * API 요청의 성공 여부, Json 직렬화 시 isSuccess로 반환하기 위해 Boolean 타입으로 선언.
 	 */
-	private final boolean isSuccess;
+	private final Boolean isSuccess;
 	/**
 	 * API 요청의 성공 결과
 	 */
@@ -33,7 +39,7 @@ public class ApiResponse<T> {
 	/**
 	 * API 요청의 오류 내용
 	 */
-	private final ErrorResponse error;
+	private final T error;
 
 	/**
 	 * API 요청 성공 시 성공 결과를 반환하기 위해 사용합니다.
@@ -46,30 +52,10 @@ public class ApiResponse<T> {
 
 	/**
 	 * API 요청 실패 시 오류를 알려주기 위해 사용합니다.
-	 * @param errorCode API 요청의 오류 코드
-	 * @param param API 요청에서 오류가 발생한 파라미터
+	 * @param errorResponse API 요청의 오류 내용
 	 * @return 오류 내용이 담긴 ApiResponse<Void>
 	 */
-	public static <Void> ApiResponse<Void> fail(ErrorCode errorCode, String param) {
-		return new ApiResponse<>(false, null, new ErrorResponse(errorCode.name(), errorCode.getMessage(), param));
-	}
-
-	/**
-	 * API 요청 실패 시 오류를 알려주기 위해 사용합니다.
-	 */
-	@AllArgsConstructor(access = AccessLevel.PRIVATE)
-	private static class ErrorResponse {
-		/**
-		 * 오류를 식별할 수 있는 오류 이름
-		 */
-		private final String name;
-		/**
-		 * 오류에 대한 설명
-		 */
-		private final String message;
-		/**
-		 * 사용자가 보낸 내용 중 오류가 발생한 파라미터
-		 */
-		private final String param;
+	public static <T> ApiResponse<T> fail(T errorResponse) {
+		return new ApiResponse<>(false, null, errorResponse);
 	}
 }
