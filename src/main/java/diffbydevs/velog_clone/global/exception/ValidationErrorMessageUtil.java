257c3b688b4bf95aae2807f1114d939f4911c932
Sooamazing <honang515@gmail.com>
2025-05-21 18:22:17 +0900

    [#15]Feat(Exception): valid exception message 한 곳에서 관리하기 위해 ValidationErrorMessageUtil 생성, Map.of로 불변 객체 생성
    - put, delete 등 수정 시도 시 runtime에 UnsupportedOperationException 발

---
+++ src/main/java/diffbydevs/velog_clone/global/exception/ValidationErrorMessageUtil.java
@@ -0,0 +1,53 @@
+package diffbydevs.velog_clone.global.exception;
+
+import java.util.Map;
+
+/**
+ * 공통으로 사용되는 Validation Error Message를 관리합니다.
+ */
+public final class ValidationErrorMessageUtil {
+
+	public static final String EMAIL = "Email";
+	public static final String MAX_WITH_FINAL_CONSONANTS = "MaxLengthWithFinalConsonants";
+	public static final String MAX_NO_FINAL_CONSONANTS = "MaxLengthNoFinalConsonants";
+	public static final String MIN_WITH_FINAL_CONSONANTS = "MinLengthWithFinalConsonants";
+	public static final String MIN_NO_FINAL_CONSONANTS = "MinLengthNoFinalConsonants";
+	public static final String NOT_NULL_WITH_FINAL_CONSONANTS = "NotNullWithFinalConsonants";
+	public static final String NOT_NULL_NO_FINAL_CONSONANTS = "NotNullNoFinalConsonants";
+	public static final String NOT_EMPTY_WITH_FINAL_CONSONANTS = "NotEmptyWithFinalConsonants";
+	public static final String NOT_EMPTY_NO_FINAL_CONSONANTS = "NotEmptyNoFinalConsonants";
+	public static final String NOT_BLANK_WITH_FINAL_CONSONANTS = "NotBlankWithFinalConsonants";
+	public static final String NOT_BLANK_NO_FINAL_CONSONANTS = "NotBlankNoFinalConsonants";
+	public static final String NULL_WITH_FINAL_CONSONANTS = "NullWithFinalConsonants";
+	public static final String NULL_NO_FINAL_CONSONANTS = "NullNoFinalConsonants";
+	public static final String PATTERN_PASSWORD = "PatternPassword";
+	public static final String SIZE_WITH_FINAL_CONSONANTS = "SizeWithFinalConsonants";
+	public static final String SIZE_NO_FINAL_CONSONANTS = "SizeNoFinalConsonants";
+	/**
+	 * Validation Error Message를 관리하는 Map입니다.
+	 * Map.ofEntries()를 사용하여 불변의 Map을 생성합니다.
+	 */
+	public static final Map<String, String> messageMap = Map.ofEntries(
+		Map.entry(EMAIL, "에 올바른 형식의 이메일 주소를 입력해 주세요."),
+		Map.entry(PATTERN_PASSWORD, "는 영문 대문자, 소문자, 숫자, 특수문자를 포함하여 8자 이상 입력해 주세요."),
+
+		// 받침 유무
+		Map.entry(MAX_WITH_FINAL_CONSONANTS, "은 최대 {value}까지 혀용합니다."),
+		Map.entry(MAX_NO_FINAL_CONSONANTS, "는 최대 {value}까지 혀용합니다."),
+		Map.entry(MIN_WITH_FINAL_CONSONANTS, "은 {value} 이상이어야 합니다."),
+		Map.entry(MIN_NO_FINAL_CONSONANTS, "는 {value} 이상이어야 합니다."),
+		Map.entry(NOT_NULL_WITH_FINAL_CONSONANTS, "은 필수입니다."),
+		Map.entry(NOT_NULL_NO_FINAL_CONSONANTS, "는 필수입니다."),
+		Map.entry(NOT_EMPTY_WITH_FINAL_CONSONANTS, "은 필수입니다. 공백을 제외한 값을 입력해 주세요."),
+		Map.entry(NOT_EMPTY_NO_FINAL_CONSONANTS, "\"는 필수입니다. 공백을 제외한 값을 입력해 주세요."),
+		Map.entry(NOT_BLANK_WITH_FINAL_CONSONANTS, "은 필수입니다. 공백을 제외한 값을 입력해 주세요."),
+		Map.entry(NOT_BLANK_NO_FINAL_CONSONANTS, "는 필수입니다. 공백을 제외한 값을 입력해 주세요."),
+		Map.entry(NULL_WITH_FINAL_CONSONANTS, "은 비어 있어야 합니다."),
+		Map.entry(NULL_NO_FINAL_CONSONANTS, "는 비어 있어야 합니다."),
+		Map.entry(SIZE_WITH_FINAL_CONSONANTS, "은 {min} ~ {max} 사이로 입력해 주세요."),
+		Map.entry(SIZE_NO_FINAL_CONSONANTS, "는 {min} ~ {max} 사이로 입력해 주세요.")
+	);
+
+	private ValidationErrorMessageUtil() {
+	}
+}
